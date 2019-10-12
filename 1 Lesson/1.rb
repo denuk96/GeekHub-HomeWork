ee80e6748c712cb4fef0fd478b230e3b3c68052f
denuk96 <e_den9611@live.com>
2019-10-12 23:39:14 +0300

    some changes

--- 1 Lesson/1.rb
+++ 1 Lesson/1.rb
@@ -1,4 +1,4 @@
 #=>  Дан целочисленный массив. Необходимо вывести вначале его элементы с четными индексами, а затем - с нечетными.
 sample = [23, 24, 25, 26]
-print sample.find_all { |elem| elem.even? } 
-print sample.find_all { |elem| elem.odd? }
+print sample.select.with_index { |elem| elem.even? } 
+print sample.select.with_index { |elem| elem.odd? }
--- 1 Lesson/2.rb
+++ 1 Lesson/2.rb
@@ -1,5 +1,5 @@
 #=>  Дан целочисленный массив. Необходимо вывести вначале его элементы с нечетными индексами, а затем - четными.
 sample = [23, 24, 25, 26]
-print sample.find_all { |elem| elem.odd? }
-print sample.find_all { |elem| elem.even? } 
+print sample.select.with_index  { |elem| elem.odd? }
+print sample.select.with_index  { |elem| elem.even? } 
 
