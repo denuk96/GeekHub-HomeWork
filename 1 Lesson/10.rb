63aaa27d57ba9c325c89202112ed8335d62c2b3a
denuk96 <54405302+denuk96@users.noreply.github.com>
2019-10-16 14:26:05 +0300

    Update 10.rb

--- 1 Lesson/10.rb
+++ 1 Lesson/10.rb
@@ -1,5 +1,13 @@
 # Дан целочисленный массив. Заменить все положительные элементы на значение максимального.
+
+# Так
 sample = [2, -10, 3, 4, 5, 100,-20]
 sample1 = sample.find_all { |elem| elem > 0}.map{sample.max}
 sample2 = sample.find_all { |elem| elem < 0}
 sample3 = sample1 + sample2 
+
+
+# или так
+sample = [2, -10, 3, 4, 5, 100,-20]
+max = sample.max
+p sample.map { |el| el > 0 ? max : el }
