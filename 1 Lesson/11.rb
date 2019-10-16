014414b8181d8276c47ead9c6253e472ce895379
denuk96 <54405302+denuk96@users.noreply.github.com>
2019-10-16 14:28:39 +0300

    Update 11.rb

--- 1 Lesson/11.rb
+++ 1 Lesson/11.rb
@@ -1,5 +1,14 @@
 # Дан целочисленный массив. Заменить все отрицательные элементы на значение минимального.
+
+# так
 sample = [2, -10, 3, 4, 5, 100,-20]
 sample1 = sample.find_all { |elem| elem < 0}.map{sample.min}
 sample2 = sample.find_all { |elem| elem > 0}
 sample3 = sample1 + sample2 
+
+
+
+# или так
+sample = [2, -10, 3, 4, 5, 100,-20]
+max = sample.min
+p sample.map { |el| el < 0 ? min : el }
